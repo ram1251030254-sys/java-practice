9fec6f9bf9ceb8865f1973bc69e844aed878314c
ram1251030254-sys <ram.1251030254@vit.edu>
2026-07-10 20:19:40 +0530

    add sum java program

---
+++ src/sum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class sum {
+    public static void main(String[] args ) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("give any two numbers: ");
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        System.out.println(num1 + num2 );
+        //similarly all operations to be performed
+
+    }
+}
